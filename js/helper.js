e9a8a346635b34f0062e828d4b95e18e93d78465
LesliePajuelo <leslie.pajuelo@hackreactor.com>
2020-08-19 09:35:56 -0700

    old site

---
+++ js/helper.js
@@ -0,0 +1,35 @@
+var HTMLheaderName = '<h1 id="name">%data%</h1>';
+var HTMLbioPic = '<img src="%data%" class="biopic">';
+var HTMLheaderRole = '<span class="white-text role">%data%</span><hr/>';
+
+var HTMLblog = '<li class="flex-item"><span class="info-text">Blog</span><span class="white-text"><a class="listContact" href="#">%data%</a></span></li>';
+var HTMLemail = '<li class="flex-item"><span class="info-text">Email</span><span class="white-text"><a class="listContact" href="mailto: #?Subject=I%20saw%20your%20awesome%20resume,%20and%20want%20you%20to%20work%20for%20me">%data%</a></span></li>';
+var HTMLlinkedIn = '<li class="flex-item"><span class="info-text">LinkedIn</span><span class="white-text"><a class="listContact" href="#" target="_blank">%data%</a></span></li>';
+var HTMLgithub = '<li class="flex-item"><span class="info-text">GitHub</span><span class="white-text"><a class="listContact" href="#" target="_blank">%data%</a></span></li>';
+var HTMLlocation = '<li class="flex-item"><span class="info-text">Location</span><span class="white-text">%data%</span></li>';
+
+var HTMLbioPic = '<img src="%data%" class="biopic">';
+
+var HTMLskillsStart = '<h3 id="skills-h3">Skills at a Glance:</h3><ul id="skills" class="flex-box"></ul>';
+var HTMLskills = '<img class="skillpic" src="%data%">';
+
+var HTMLprojectStart = '<div class="project-entry"></div>';
+var HTMLprojectTitle = '<a href="#" target="_blank">%data%</a>';
+var HTMLprojectTagline = '<div class="tagline">%data%</div>';
+var HTMLprojectURLs = '<div class="URL-text">%data%</div>';
+var HTMLprojectDescription = '<p><br>%data%</p>';
+var HTMLprojectImage = '<a href="#" target="_blank"><img class="projpic" src="%data%"></a>';
+
+var HTMLschoolStart = '<div class="education-entry"></div>';
+var HTMLschoolName = '<a href="#" target="_blank">%data%';
+var HTMLschoolDegree = ' — %data%</a>';
+var HTMLschoolDates = '<div class="date-text">%data%</div>';
+var HTMLschoolLocation = '<div class="location-text">%data%</div>';
+var HTMLschoolMajor = '<br>Major: %data%';
+var HTMLschoolMinor = '<br>Minor: %data%';
+
+var HTMLonlineClasses = '<div class="education-entry"><h3>Special</h3></div>';
+var HTMLonlineTitle = '<a href="#" target="_blank">%data%';
+var HTMLonlineSchool = ' — %data%</a>';
+var HTMLonlineDates = '<div class="date-text">%data%</div>';
+var HTMLonlineURL = '<br><a class="courseURl" href="#" target="_blank>%data%</a>';
